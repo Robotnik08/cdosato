52a3cb043b5c710d361b88859cda4464510ae527
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2025-06-24 04:12:32 +0200

    added writeBytes and appendBytes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
   <img src="assets/dosato_logo_wide.png">
 </p>
 
-# Dosato version 0.6.10
+# Dosato version 0.7.0
 
 cDosato is the official implementation of the DOSATO programming language.<br>
 
--- include/standard_libraries/std_io.h
+++ include/standard_libraries/std_io.h
@@ -11,7 +11,9 @@ Value io_listen (ValueArray args, bool debug);
 // file io functions
 Value io_read_file (ValueArray args, bool debug);
 Value io_write_file (ValueArray args, bool debug);
+Value io_write_bytes_file (ValueArray args, bool debug);
 Value io_append_file (ValueArray args, bool debug);
+Value io_append_bytes_file (ValueArray args, bool debug);
 Value io_delete_file (ValueArray args, bool debug);
 Value io_file_exists (ValueArray args, bool debug);
 Value io_move_file (ValueArray args, bool debug);
